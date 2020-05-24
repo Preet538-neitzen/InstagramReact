888da55996cc93a3589152646d37e9641781afbb
Preet538-neitzen <sanghavipreet2001@gmail.com>
2020-05-24 23:32:57 +0530

    Updates

---
+++ precache-manifest.93d108ed3c623dc5dc9658231af1518b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad4ba4bca801bb15c1ada06bb2d551f",
+    "url": "/InstaReact/index.html"
+  },
+  {
+    "revision": "41641349b9535f4570bb",
+    "url": "/InstaReact/static/css/main.acef5122.chunk.css"
+  },
+  {
+    "revision": "fd1bd86322f7d374e757",
+    "url": "/InstaReact/static/js/2.489f75e1.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/InstaReact/static/js/2.489f75e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41641349b9535f4570bb",
+    "url": "/InstaReact/static/js/main.3d909716.chunk.js"
+  },
+  {
+    "revision": "435fae0c2c9c51a40272",
+    "url": "/InstaReact/static/js/runtime-main.5c447757.js"
+  }
+]);
